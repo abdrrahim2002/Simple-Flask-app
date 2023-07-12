310db89eed64f654c890b8e1080fdbc6f706b09f
ABD RRAHIM <python.py2002@gmail.com>
2023-07-12 18:31:04 +0100

    first comint

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def lol():
+    return render_template('page2.html')
+
+if __name__ == '__main__':
+    app.run(debug=False, port='0.0.0.0')
